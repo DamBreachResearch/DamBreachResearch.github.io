734b4d26ccce8be59d1fa5f44ed4d81f81429240
mstphayassin <mstphayassin@gmail.com>
2025-11-28 12:30:31 -0700

    deploy to pages

--- docs/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"06F2oTUgKkvDN219VtVFWqNsGxbYDHxBG2bibNU4HKQ=\"\n}"
--- docs/dev/static/chunks/app_page_tsx_607b5035._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_next_3cff121f._.js",
-  "static/chunks/_c86ad7c7._.js"
-],
-    source: "dynamic"
-});

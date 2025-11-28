d305255a7a18f7839de5acca96c0023cb20212b8
mstphayassin <mstphayassin@gmail.com>
2025-11-28 11:33:01 -0700

    deploy github pages

---
+++ README.md
@@ -0,0 +1,1 @@
+DamBreachResearch.github.io
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // config here
+  distDir: 'dist',
+  output: 'export',
 };
 
 export default nextConfig;
